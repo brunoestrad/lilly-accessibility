50cb1bd96432853c89bde8844533509b9ddb9abf
Sahid Munjavar <sahidmunjavar.s@gmail.com>
2024-11-05 11:07:59 +0000

    final init

---
+++ signSync/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (<>
+
+
+    <h1>Hello WOrld</h1>
+    <Button >Hello World</Button>
+  </>
+  );
+}
